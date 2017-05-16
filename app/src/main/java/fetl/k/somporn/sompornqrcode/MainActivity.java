5d9af25053073844c17c542670ba8557a389a125
sama <somporn.k@th.fujikura.com>
2017-05-16 11:24:53 +0700

    Intent to register

--- app/src/main/java/fetl/k/somporn/sompornqrcode/MainActivity.java
+++ app/src/main/java/fetl/k/somporn/sompornqrcode/MainActivity.java
@@ -1,13 +1,62 @@
 package fetl.k.somporn.sompornqrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //explicite ประกาศตัวแปล
+    private EditText userEditText, passEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View is การผูกความสัมพัน ตัวแปล กับ in wiew
+        initialView();
+        //controller
+        controller();
+
+    }  // main method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.editUser);
+        passEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For textview
+        if (v == textView) {
+            //Intent to Register
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+
+        }
+       //for buttom
+        if (v ==button) {
+
+        }
+
+    }
+
+
+
+}   // Main Class คลาสหลัก
