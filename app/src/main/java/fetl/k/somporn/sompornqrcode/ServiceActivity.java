6bbb86b01d5abedd45fc42211ef17e9649ba3056
sama <somporn.k@th.fujikura.com>
2017-05-18 09:45:29 +0700

    show text ok

--- app/src/main/java/fetl/k/somporn/sompornqrcode/ServiceActivity.java
+++ app/src/main/java/fetl/k/somporn/sompornqrcode/ServiceActivity.java
@@ -2,12 +2,35 @@ package fetl.k.somporn.sompornqrcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import static fetl.k.somporn.sompornqrcode.R.id.livProduct;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicite
+    private TextView textView;
+    private ImageView barImageView, qrImageView;
+    private ListView listView;
+    private String nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        //Initial View
+        initialView();
+        //Show Text
+        nameString = getIntent().getStringExtra("Login");
+        textView.setText(nameString);
+    }
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtNameLogin);
+        barImageView = (ImageView) findViewById(R.id.imvBarCode);
+        qrImageView = (ImageView) findViewById(R.id.imvQrCode);
+        listView = (ListView) findViewById(R.id.livProduct);
     }
 }
